e2bf0667b981efdbbaf1553537cb231126972532
Vanderlei Neres <vanderlei_nsilva@hotmail.com>
2024-08-03 19:44:18 -0300

    Criação do Método Post. Implementando ResponseEntity. Customizando resposta XML com Wrapper

--- src/main/java/com/treinamento/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/treinamento/algafood/api/controller/CozinhaController.java
@@ -3,11 +3,18 @@ package com.treinamento.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.treinamento.algafood.api.model.CozinhasXmlWrapper;
 import com.treinamento.algafood.domain.model.Cozinha;
 import com.treinamento.algafood.domain.repository.CozinhaRepository;
 
@@ -23,8 +30,25 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if(cozinha == null) {
+			return ResponseEntity.notFound().build(); 
+		}
+		
+		return ResponseEntity.ok(cozinha);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha salvar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
 	}
 }
