8c2e530a82aa9b749e45282289d4635a9286be33
Vanderlei Neres <vanderlei_nsilva@hotmail.com>
2024-08-04 19:20:36 -0300

    Implementado recursos de Restaurante

--- src/main/java/com/treinamento/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/treinamento/algafood/api/controller/RestauranteController.java
@@ -2,15 +2,23 @@ package com.treinamento.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.treinamento.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.treinamento.algafood.domain.model.Restaurante;
 import com.treinamento.algafood.domain.repository.RestauranteRepository;
+import com.treinamento.algafood.domain.service.RestauranteService;
 
 @RestController()
 @RequestMapping("/restaurantes")
@@ -18,20 +26,48 @@ public class RestauranteController {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
+	@Autowired
+	private RestauranteService restauranteService;
+
 	@GetMapping
 	public List<Restaurante> listar() {
 		return restauranteRepository.listar();
 	}
-	
+
 	@GetMapping("/{restauranteId}")
 	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
-		
-		if(restaurante != null) {
+
+		if (restaurante != null) {
 			return ResponseEntity.ok(restaurante);
 		}
-		
+
 		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Restaurante adicionar(@RequestBody Restaurante restaurante) {
+		return restauranteService.salvar(restaurante);
+	}
+
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+		try {
+			Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+
+			if (restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+				restauranteAtual = restauranteService.salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual);
+			}
+			
+			return ResponseEntity.notFound().build();
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 }
---
+++ src/main/java/com/treinamento/algafood/domain/service/RestauranteService.java
@@ -0,0 +1,38 @@
+package com.treinamento.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.treinamento.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.treinamento.algafood.domain.model.Cozinha;
+import com.treinamento.algafood.domain.model.Restaurante;
+import com.treinamento.algafood.domain.repository.CozinhaRepository;
+import com.treinamento.algafood.domain.repository.RestauranteRepository;
+
+@Service
+public class RestauranteService {
+
+	@Autowired
+	RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	CozinhaRepository cozinhaRepository;
+	
+	public Restaurante salvar (Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if(cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+		
+		restaurante.setCozinha(cozinha);
+		
+		return restauranteRepository.salvar(restaurante);
+	}
+	
+	
+	
+	
+}
