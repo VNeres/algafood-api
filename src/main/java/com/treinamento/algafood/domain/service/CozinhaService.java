dffab023b74f997401ad2a4057905ab5aa1ca8fc
Vanderlei Neres <vanderlei_nsilva@hotmail.com>
2024-10-28 16:56:09 -0300

    Refatorando classe Cozinha para uso do SDJ

--- src/main/java/com/treinamento/algafood/domain/service/CozinhaService.java
+++ src/main/java/com/treinamento/algafood/domain/service/CozinhaService.java
@@ -2,7 +2,6 @@ package com.treinamento.algafood.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.treinamento.algafood.domain.exception.EntidadeEmUsoException;
@@ -15,20 +14,21 @@ public class CozinhaService {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	public Cozinha salvar(Cozinha cozinha) {
-		return cozinhaRepository.salvar(cozinha);
+		return cozinhaRepository.save(cozinha);
 	}
-	
+
 	public void excluir(Long cozinhaId) {
 		try {
-			cozinhaRepository.remover(cozinhaId);
-			
-		} catch(EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
-			
-		} catch(DataIntegrityViolationException e) {
+
+			if (!cozinhaRepository.existsById(cozinhaId)) {
+				throw new EntidadeNaoEncontradaException(
+						String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
+			}
+			cozinhaRepository.deleteById(cozinhaId);
+
+		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
 					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
 		}
