2a34b63ba4ae2119fbc032b1cc80c5288573d892
Vanderlei Neres <vanderlei_nsilva@hotmail.com>
2025-02-06 19:54:31 -0300

    Concluindo aula 5.11

--- src/main/java/com/treinamento/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/treinamento/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,8 @@
 package com.treinamento.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.treinamento.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+
+	List<Cozinha> findTodasByNomeContaining(String nome);
+	Optional<Cozinha> findByNome(String nome);
+	boolean existsByNome(String nome);
 	
 }
--- src/main/java/com/treinamento/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/treinamento/algafood/domain/repository/RestauranteRepository.java
@@ -1,12 +1,20 @@
 package com.treinamento.algafood.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.treinamento.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
-	
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 
+	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	List<Restaurante> consultarPorNome(String nome, Long cozinhaId);
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	int countByCozinhaId(Long cozinhaId);
 }
