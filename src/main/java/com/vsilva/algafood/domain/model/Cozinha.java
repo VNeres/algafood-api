22bad00168235a854f85e8e63792fd7cb3f1f2bf
VNeres_ <vanderlei_nsilva@hotmail.com>
2026-01-13 05:01:07 -0300

    Adicionando lombok

--- src/main/java/com/vsilva/algafood/domain/model/Cozinha.java
+++ src/main/java/com/vsilva/algafood/domain/model/Cozinha.java
@@ -1,55 +1,26 @@
 package com.vsilva.algafood.domain.model;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity /*(name="tab_cozinhas") - se quiser definir o nome da tabela*/
 public class Cozinha {
 
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column /*(name="nom_cozinha") - se qusier definir o nome da coluna)*/
 	private String nome;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		return Objects.equals(id, other.id);
-	}
 	
 	
 }
