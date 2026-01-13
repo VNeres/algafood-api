0799eac9b6135ec033beda785ac20a2cb53ecda3
VNeres_ <vanderlei_nsilva@hotmail.com>
2026-01-13 05:12:21 -0300

    Adicionando repository de restaurante

---
+++ src/main/java/com/vsilva/algafood/domain/repository/RestauranteRepository.java
@@ -0,0 +1,13 @@
+package com.vsilva.algafood.domain.repository;
+
+import java.util.List;
+
+import com.vsilva.algafood.domain.model.Restaurante;
+
+public interface RestauranteRepository {
+
+	List<Restaurante> todos();
+	Restaurante porId(Long id);
+	Restaurante adicionar(Restaurante restaurante);
+	void remover(Restaurante restaurante);
+}
---
+++ src/main/java/com/vsilva/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.vsilva.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import com.vsilva.algafood.domain.model.Restaurante;
+import com.vsilva.algafood.domain.repository.RestauranteRepository;
+
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+	@PersistenceContext
+	EntityManager manager;
+	
+	@Override
+	public List<Restaurante> todos() {
+		TypedQuery<Restaurante> query = manager.createQuery("from Restaurante", Restaurante.class);
+		
+		return query.getResultList();
+	}
+
+	@Override
+	public Restaurante porId(Long id) {
+		
+		return manager.find(Restaurante.class, id);
+	}
+
+	@Transactional
+	@Override
+	public Restaurante adicionar(Restaurante restaurante) {
+		
+		return manager.merge(restaurante);
+	}
+
+	@Transactional
+	@Override
+	public void remover(Restaurante restaurante) {
+		
+		restaurante = porId(restaurante.getId());
+		manager.merge(restaurante);
+		
+	}
+
+}
