d39db15e5f71b824cc8d1f0e24a48336ec99a8a9
VNeres_ <vanderlei_nsilva@hotmail.com>
2026-01-13 01:07:24 -0300

    Criando "LIstar Cozinha" com JPA

--- src/main/java/com/vsilva/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/vsilva/algafood/jpa/CadastroCozinha.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Component;
 
@@ -17,8 +18,16 @@ public class CadastroCozinha {
 	private EntityManager manager;
 	
 	public List<Cozinha> listar() {
+		//select * from Cozinha
 		TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
 		
 		return query.getResultList();
+	}
+	
+	@Transactional // anotação para fazer com que o método seja executado dentro de uma transação.
+	public Cozinha adicionar(Cozinha cozinha) {
+		// insert into Cozinha (nome) values ('value')
+		
+		return manager.merge(cozinha);
 	}
 }
