1e2927436ab5d5de1f63679a202f2c09af47e1ba
VNeres_ <vanderlei_nsilva@hotmail.com>
2026-01-27 21:41:43 -0300

    Conhecendo e testando os prefixos de Query Methods

--- src/main/java/com/vsilva/algafood/api/controller/TesteController.java
+++ src/main/java/com/vsilva/algafood/api/controller/TesteController.java
@@ -1,6 +1,8 @@
 package com.vsilva.algafood.api.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vsilva.algafood.domain.model.Cozinha;
+import com.vsilva.algafood.domain.model.Restaurante;
 import com.vsilva.algafood.domain.repository.CozinhaRepository;
+import com.vsilva.algafood.domain.repository.RestauranteRepository;
 
 @RestController
 @RequestMapping("/teste")
@@ -18,8 +22,46 @@ public class TesteController {
 	@Autowired
 	CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinha/por-nome")
-//	public List<Cozinha> buscarPorNome(@RequestParam String nome) {
-//		return cozinhaRepository.porNome(nome);
-//	}
+	@Autowired
+	RestauranteRepository restauranteRepository;
+	
+	@GetMapping("/cozinha/por-nome")
+	public List<Cozinha> buscarPorNome(@RequestParam String nome) {
+		return cozinhaRepository.findTodasByNomeContaining(nome);
+	}
+	
+	@GetMapping("/cozinha/unica-por-nome")
+	public Optional<Cozinha> buscarUnicaPorNome(@RequestParam String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
+	
+	@GetMapping("/restaurante/por-taxa-frete")
+	public List<Restaurante> buscarEntreTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+	}
+	
+	@GetMapping("/restaurante/por-nome-cozinha-id")
+	public List<Restaurante> buscarPorNomeCozinhaId(String nome, Long cozinhaId) {
+		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+	}
+	
+	@GetMapping("/restaurante/por-primeiro-nome")
+	public Optional<Restaurante> buscarPrimeiroPorNome(String nome) {
+		return restauranteRepository.findFirstByNomeContaining(nome);
+	}	
+	
+	@GetMapping("/restaurante/por-top2-nome")
+	public List<Restaurante> buscarPorTop2Nome(String nome) {
+		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("/cozinha/exists-nome")
+	public boolean verificarExisteNome(String nome) {
+		return cozinhaRepository.existsByNome(nome);
+	}
+	
+	@GetMapping("/restaurante/exists-por-cozinha")
+	public int countPorCozinhaId(Long cozinhaId) {
+		return restauranteRepository.countByCozinhaId(cozinhaId);
+	}
 }
--- src/main/java/com/vsilva/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/vsilva/algafood/domain/repository/CozinhaRepository.java
@@ -1,11 +1,18 @@
 package com.vsilva.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vsilva.algafood.domain.model.Cozinha;
 
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
 	
-	//List<Cozinha> porNome(String nome);
+	List<Cozinha> findTodasByNomeContaining(String nome);
+	
+	Optional<Cozinha> findByNome(String nome);
+	
+	boolean existsByNome(String nome);
 
 }
