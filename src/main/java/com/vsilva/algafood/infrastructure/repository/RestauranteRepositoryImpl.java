74b99bb01c78cc3d76530af00ac2ac8b4cf6ab02
VNeres_ <vanderlei_nsilva@hotmail.com>
2026-01-31 11:35:46 -0300

    Adicionando filtros dinâmicos com predicates

--- src/main/java/com/vsilva/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/vsilva/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.vsilva.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -8,8 +9,12 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
+import org.springframework.data.util.Predicates;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.vsilva.algafood.domain.model.Restaurante;
 import com.vsilva.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -26,7 +31,23 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		List<Predicate> predicates = new ArrayList<Predicate>();
+		
+		if(StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
+		
+		if(taxaInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaInicial));
+		}
+		
+		if(taxaFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFinal));
+		}
+				
+		criteria.where(predicates.toArray(new Predicate[0]));
 
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
