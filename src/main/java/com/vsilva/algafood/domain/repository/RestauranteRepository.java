c045188a2c5c67a0cd2fc409a87c4df5fe87d5ce
VNeres_ <vanderlei_nsilva@hotmail.com>
2026-03-27 18:40:19 -0300

    Resolvendo problema de N+1

--- src/main/java/com/vsilva/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/vsilva/algafood/domain/repository/RestauranteRepository.java
@@ -5,11 +5,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.vsilva.algafood.domain.model.Restaurante;
 
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
+	
+	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	List<Restaurante> findAll();
 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
