067494dedcb7fe24688af8143addc63eb754e098
VNeres_ <vanderlei_nsilva@hotmail.com>
2026-08-31 22:14:52 -0300

    Adicionando dados de testes com callback do Flyway

--- src/main/java/com/vsilva/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/vsilva/algafood/domain/repository/RestauranteRepository.java
@@ -12,7 +12,7 @@ import com.vsilva.algafood.domain.model.Restaurante;
 
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 	
-	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha")
 	List<Restaurante> findAll();
 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
